5ffcb1a969825532b8016d8f2efa8ef6d92ee16f
kormakursnorra <kormakur02@gmail.com>
2026-03-04 19:03:21 +0000

    Moved boss into hobo folder

--- src/hobos/hobo.h
+++ src/hobos/hobo.h
@@ -1,10 +1,17 @@
 #ifndef HOBO_H
 #define HOBO_H
 
-#include "creatures/"
+#include "zoos/zoo.h"
+// #include "items.h"
+
+#include <string>
 
 class Hobo {
 public:
+    const std::string name;
+    static Zoo *zoo;
+    // static Items *items;   
+
     
     void chooseAction();
 };
